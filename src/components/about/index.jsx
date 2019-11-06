69871c84299a60c37ebdb1771c1f85e22b2ea72b
Denner09 <denner.sr@gmail.com>
2019-11-06 13:15:34 -0400

    Alteração sobre

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <h3 className="login-heading mb-4">Sobre</h3>
             <img className="w-75 mx-auto d-block" src={ `${process.env.PUBLIC_URL}/logo.png` } alt=""/>
             <p className="text-justify">
-                To Event é um sistema para controle de frequências e vagas, como também de inscrições focado em eventos. 
+                To Event é um sistema para controle de frequências e vagas, como também de inscrições focado em evento. 
                 O sistema foi construido dentro do projeto Fábrica de Software.
                 O projeto é realizado dentro do laboratório LADESP situada na Universidade Federal de Mato Grosso do Sul de Corumbá.
             </p>
